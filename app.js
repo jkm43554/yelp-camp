09fbd04c72e70123356a6028c0d7f1d8039298ea
Jacob Martin <you@example.com>
2019-06-14 19:46:50 +0000

    add database to mongodb atlas

--- app.js
+++ app.js
@@ -19,7 +19,14 @@ var commentRoutes = require('./routes/comments'),
 
 
 // seedDB()
-mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://devSprout:JKmm596788@cluster0-xxs56.mongodb.net/test?retryWrites=true&w=majority', { 
+	useNewUrlParser: true,
+	iseCreateIndex: true
+}).then(()=>{
+	console.log('Connected to DB!');
+}).catch(err =>{
+	console.log('ERROR:',err.message);
+});
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
